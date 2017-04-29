765da8827b70a0221199d26a7fe70ecc70f47c0f
arnasziedas <arnas.ziedavicius@hotmail.com>
2017-04-29 18:55:00 +0100

    Add npm and gulpfile config files

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    bourbon = require('node-bourbon').includePaths,
+    livereload = require('gulp-livereload');
+
+var onError = function (err) {  
+  gutil.beep();
+  console.log(err);
+};
+
+var paths = {
+ styles: ['./styles/**/*.scss'],
+ scripts: ['./scripts/**/*.js'],
+};
+
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(plumber(onError))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./assets/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/js/'))
+    .pipe(livereload());
+});
+
+gulp.task('styles', function() {
+  gulp.src(paths.styles)
+    .pipe(plumber(onError))  
+    .pipe(sass({
+      includePaths: bourbon,
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css/'))
+    .pipe(livereload());
+});
+
+//Watch task
+gulp.task('default', function() {
+  livereload.listen();
+  // Watch css
+  gulp.watch(paths.styles, ['styles']);
+  // Watch js
+  gulp.watch(paths.scripts, ['scripts']);
+});
